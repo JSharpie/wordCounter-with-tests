0e750f1fd0573618f70af14430b261f7d5a6bdc5
Jacob Sharp <sidewayseightgmod@gmail.com>
2015-11-16 08:13:35 -0500

    first commit

---
+++ read.js
@@ -0,0 +1,44 @@
+var readTime = {
+  wpm: 0,
+  configWPM: function(setWpm){
+    readTime.wpm = setWpm;
+  },
+  getWPM: function(setWpm){
+    return readTime.wpm;
+  },
+  convertToArray: function(target){
+    var elements = [].slice.call(target);
+    console.log(elements);
+    return elements;
+  },
+  getTextFromNodes: function(target){
+    var arr = readTime.convertToArray(target);
+    var newArr = [];
+    _.each(arr, function(el){
+      newArr += el.innerText.split(',');
+    });
+    return newArr;
+  },
+  removePunctuation: function(target){
+    function punc(val){
+      return (val != '!' && val != '?' && val != '.' && val != ',');
+    }
+    var returnStr = target.split('').filter(punc).join('');
+    return returnStr;
+  },
+  countWords: function(target){
+    var arr = target.split(' ');
+    _.each(arr, function(el){
+      readTime.removePunctuation(el);
+    });
+    var count = arr.length;
+    return count;
+  },
+  calcWPM: function(target){
+    return Math.round(target/readTime.wpm) + " minute read";
+  },
+  words: function(target){
+    var wordsOnPage = readTime.countWords(readTime.getTextFromNodes(readTime.convertToArray(target)));
+    return readTime.calcWPM(wordsOnPage);
+  }
+};
